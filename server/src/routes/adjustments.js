f4a67e12c3cee5fbb32ae70bd2972b290964867b
Lakshya Modi <lakshyamodi@Lakshyas-MacBook-Air.local>
2026-05-14 14:06:42 +0530

    debug

--- server/src/routes/adjustments.js
+++ server/src/routes/adjustments.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const managerOnly = require("../middleware/managerOnly");
-const { getAll, create, validate, remove } = require("../controllers/adjustmentController");
 const { getAll, getOne, create, validate, remove } = require("../controllers/adjustmentController");
+
 router.get("/:id", auth, getOne);
 router.get("/",              auth,            getAll);
 router.post("/",             auth,            create);
