b4f0347e2ee6b94e99befe7e9cbb61d58cf32147
Lakshya Modi <lakshyamodi@Lakshyas-MacBook-Air.local>
2026-05-14 12:54:36 +0530

    debug

--- server/src/routes/transfers.js
+++ server/src/routes/transfers.js
@@ -4,10 +4,9 @@ const auth = require("../middleware/auth");
 const managerOnly = require("../middleware/managerOnly");
 const { getAll, create, validate, remove } = require("../controllers/transferController");
 
-router.get("/",              auth,            getAll);
-router.get("/:id",           auth,            getOne);
-router.post("/",             auth,            create);
+router.get("/",              auth,             getAll);
+router.post("/",             auth,             create);
 router.post("/:id/validate", auth, managerOnly, validate);
-router.delete("/:id", auth, managerOnly, remove);
+router.delete("/:id",        auth, managerOnly, remove);
 
 module.exports = router;
